0b8beb28f6088732455c586a733d68907744147c
Jaclyn Hall <jaclynchall@gmail.com>
2018-07-20 19:11:24 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,10 +14,10 @@ def monopoly_with_second_tier
 	monopoly = {railroads: {
 		pieces: 4,
 		names: {
-			
+
 		},
 		rent_in_dollars: {
-			
+
 		},
 		}
 	}
@@ -27,10 +27,13 @@ def monopoly_with_third_tier
 	monopoly = {railroads: {
 		pieces: 4,
 		names: {
-			
+
 		},
 		rent_in_dollars: {
-			
+			one_piece_owned: 25,
+			two_pieces_owned: 50,
+			three_pieces_owned: 100,
+			four_pieces_owned: 200
 		},
 		}
 	}
